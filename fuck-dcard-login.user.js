6b108c454c9f4b2e6aaf950dd716ecc3a1926f1d
Zong Jhe Wu <s25g5d4@gmail.com>
2021-03-30 13:54:07 +0800

    Add the script

---
+++ fuck-dcard-login.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        FuckDcardLogin
+// @description 小孩子才登入看文章。低能卡吃屎。
+// @namespace   https://github.com/s25g5d4/e-hentai-retriever
+// @match       https://*.dcard.tw/*
+// @match       http://*.dcard.tw/*
+// @version     1.0
+// @author      s25g5d4
+// @homepageURL https://github.com/s25g5d4/FuckDcardLogin
+// @grant       none
+// ==/UserScript==
+
+const removeLoginModal = (target) => {
+    target.style.position = null;
+    target.style.top = null;
+    target.querySelector('.__portal').remove();
+  };
+
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach(record => {
+      const style = record.target.getAttribute('style');
+      console.log('style changed!', style);
+
+      if (style.includes('position') && style.includes('top')) {
+        observer.disconnect();
+        removeLoginModal(record.target);
+      }
+    });
+  });
+
+  const target = document.body;
+  observer.observe(target, { attributes : true, attributeFilter : ['style'] });
