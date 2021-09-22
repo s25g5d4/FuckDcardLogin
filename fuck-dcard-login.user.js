e41988dd9e894043e9c2c1238639b513f3b7c80d
Zong Jhe Wu <s25g5d4@gmail.com>
2021-09-22 14:29:14 +0800

    Fix for dcard website update

--- fuck-dcard-login.user.js
+++ fuck-dcard-login.user.js
@@ -11,22 +11,24 @@
 // ==/UserScript==
 
 const removeLoginModal = (target) => {
-    target.style.position = null;
-    target.style.top = null;
+    target.style.overflow = null;
     target.querySelector('.__portal').remove();
-  };
+};
 
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach(record => {
-      const style = record.target.getAttribute('style');
-      console.log('style changed!', style);
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((record) => {
+        const style = record.target.getAttribute('style');
+        console.log('style changed!', style);
 
-      if (style.includes('position') && style.includes('top')) {
-        observer.disconnect();
-        removeLoginModal(record.target);
-      }
+        if (style.includes('overflow')) {
+            observer.disconnect();
+            removeLoginModal(record.target);
+        }
     });
-  });
+});
 
-  const target = document.body;
-  observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+const target = document.body;
+observer.observe(target, { attributes: true, attributeFilter: ['style'] });
+setTimeout(() => {
+    target.querySelector('.__portal').remove();
+}, 100);
